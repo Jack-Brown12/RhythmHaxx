0ce2a494e79578a8550933344bde57c7ab38d643
andriibessarab <andriibessarab@gmail.com>
2025-10-25 15:30:32 -0400

    Optimize scaling, translating and boundary-finding

--- server/algorithms.py
+++ server/algorithms.py
@@ -1,6 +1,5 @@
 import overpy
 
-api = overpy.Overpass()
 query = """
 [out:json][timeout:25];
 (
@@ -10,62 +9,48 @@ out body;
 >;
 out skel qt;
 """
-result = api.query(query)
-print(result.ways)
-def get_map_path_coordinates(initial_point, scaling_factor, points):
-    #print("Initial Point:", initial_point)
-    #print("Scaling Factor:", scaling_factor)
-    #print("Points:", points)
+BOUNDARY_PADDING = 2
 
-    min_x = float('inf')
-    max_x = float('-inf')
-    min_y = float('inf')
-    max_y = float('-inf')
-    bounds = [min_x,max_x,min_y,max_y]
+
+def get_map_path_coordinates(initial_point, scaling_factor, points):
+    api = overpy.Overpass()
+    result = api.query(query)
+    print(result.ways)
+
+    # min x, max x, min y, max y
+    bounds = [float('inf'), float('-inf'), float('inf'), float('-inf')]
 
     # Scale, translate and find bounds
-    points = scale_point(points,scaling_factor)
-    points,bounds = translate_point(points,initial_point,bounds)
-    print(points)
-    print(bounds)
-    
+    for i in range(len(points)):
+        # Scale
+        points[i][0] *= scaling_factor
+        points[i][1] *= scaling_factor
 
-    # Find closest node to starting point
+        # Translate
+        points[i][0] += initial_point[0]
+        points[i][1] += initial_point[1]
 
-    # Run DFS to find best-matching path
+        # Update bounds
+        bounds[0] = min(bounds[0], points[i][0])
+        bounds[1] = max(bounds[1], points[i][0])
+        bounds[2] = min(bounds[2], points[i][1])
+        bounds[3] = max(bounds[3], points[i][1])
+
+    # Add padding to bounds
+    bounds[0] -= BOUNDARY_PADDING
+    bounds[1] += BOUNDARY_PADDING
+    bounds[2] -= BOUNDARY_PADDING
+    bounds[3] += BOUNDARY_PADDING
+
+    # Run DFS to find a best-matching path
 
     # Return coordinates
     
     return { "path_coordinates": [1,1] }
 
 
-
-def scale_point(point, scaling_factor):
-    for i in range(len(point)):
-            point[i][0] *= scaling_factor
-            point[i][1] *= scaling_factor
- 
-    return point
-    
-
-def translate_point(point, initial_point, bounds):
-    for i in range(len(point)):
-        point[i][0] += initial_point[0]
-        point[i][1] += initial_point[1]
-        bounds[0] = min(bounds[0],point[i][0]-2)
-        bounds[1] = max(bounds[1],point[i][0]+2)
-        bounds[2] = min(bounds[2],point[i][1]-2)
-        bounds[3] = max(bounds[3],point[i][1]+2)
-    return point,bounds
-
 def pulldata(bounds):
      pass
-     
 
-    
+
 get_map_path_coordinates([4,4],3,[[0,0],[0,2],[2,2],[2,0]])
-    
-
-
-
-
